df252b6c2929e8d6c1d9b421c517ff14ed558dc0
Inspired9k <72418473+Inspired9k@users.noreply.github.com>
2020-10-06 00:27:00 +0200

    Add files via upload

---
+++ ЛАБА 4.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;// количество элементов в последовательности  
+	scanf_s("%d", &n);
+	int k; //число в последовательности 
+	int comp = 1;// произведение чисел % 5 == 0
+	int pos = -1;// позиция min числа / на 5
+	int mindigit = 999999;// минимальное число / на 5
+	
+	for (int i = 1; i <= n; i++)
+	{
+		scanf_s("%d", &k);
+		if (k % 5 == 0)
+		{
+			comp = comp * k;
+			if (k < mindigit)
+			{
+				mindigit = k;
+				pos = i;
+			}
+		}
+	}
+	
+	printf("%d, %d , %d", comp, pos, mindigit);
+}
